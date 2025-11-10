ec41fadc6900ddc27c389d2c438d72694f06233a
VitorPio7 <vitorvpio60@gmail.com>
2025-11-10 16:59:42 -0300

    feat: i did this today

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Detector import *
 import os
 
+##Estamos usando o SSD MobileNet v3 do tensorFlow, carregado via OpenCV
+##Ele foi pré-treinado no dataset COCO
+
 def main():
-    videoPath =0 ## para usar webcam podemos colocar 0 e para videos já gravados é necessário colocar o path dele
+    videoPath = "test_videos/cozinhando.mp4" ## para usar webcam podemos colocar 0 e para videos já gravados é necessário colocar o path dele
     
     configPath = os.path.join("model_data","ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
     modelPath = os.path.join("model_data","frozen_inference_graph.pb")
